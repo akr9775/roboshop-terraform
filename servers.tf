9b453803dc2af5832dd401b0d8487c0be3d9c11f
akr9757 <anilkumareddy9757@gmail.com>
2023-12-14 18:14:30 +0530

    roboshop shell script

--- servers.tf
+++ servers.tf
@@ -39,3 +39,79 @@ resource "aws_route53_record" "frontend" {
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
+
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = "catalogue"
+  }
+
+}
+
+resource "null_resource" "provisioner" {
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.catalogue.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/akr9775/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash catalogue.sh"
+    ]
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z068357614KDH6BL7CVGP"
+  name    = "catalogue-dev.akrdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = "mongodb"
+  }
+
+}
+
+resource "null_resource" "provisioner" {
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.mongodb.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/akr9775/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash mongodb.sh"
+    ]
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z068357614KDH6BL7CVGP"
+  name    = "catalogue-dev.akrdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
